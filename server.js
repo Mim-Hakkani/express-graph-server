970d77b80c7b1d95376cdb87dd88227f71ca84e4
32-Mimhakkani <mim.cse32@gmail.com>
2022-03-14 11:40:53 +0600

    setrup the port and expres-graphql error

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 4000
+// import express from 'express';
+// const expressGraphQL = require('express-graphql').graphqlHTTP
+// const app = express()
+// const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// app.use('/graphql',expressGraphQL({
+//     graphiql:true
+// }));
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World! the hakkani ')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+
+const express = require('express');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+
+
+const app = express();
+
+app.use('/graphql', expressGraphQL({
+    graphiql:true
+}));
+
+app.listen(4000, () => {
+    console.log('Listning');
 })
